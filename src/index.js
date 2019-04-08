4b36865fc6b69078228fd0737cedec2a02767d50
Caleb Panza <caleb.panza@icloud.org>
2019-04-08 14:25:00 -0400

    Fixing npm publishing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import * as AccordionComp from './Accordion'
-import * as CallsToActionComp from './CallsToAction'
-import * as GeneralContentComp from './GeneralContent'
-import * as LoadersComp from './Loaders'
-import * as MediaComp from './Media'
+import * as Accordion from './Accordion'
+import * as CallsToAction from './CallsToAction'
+import * as GeneralContent from './GeneralContent'
+import * as Loaders from './Loaders'
+import * as Media from './Media'
 
 
-export const Accordion = AccordionComp
-export const CallsToAction = CallsToActionComp
-export const GeneralContent = GeneralContentComp
-export const Loaders = LoadersComp
-export const Media = MediaComp
+export { Accordion, CallsToAction, GeneralContent, Loaders, Media }
