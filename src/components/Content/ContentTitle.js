2dd98221425fa4c9e037f890679c64437d170dea
Caleb Panza <caleb.panza@icloud.org>
2019-05-31 09:23:00 -0400

    Added the subtitle component
    Also updated some random stuff in the ContentTitle

--- src/components/Content/ContentTitle.js
+++ src/components/Content/ContentTitle.js
@@ -10,10 +10,4 @@ const ContentTitle = ({ className, children, larger = false }) => (
     </h2>
 )
 
-const defaultProps = {};
-const propTypes = {};
-
-ContentTitle.defaultProps = defaultProps;
-ContentTitle.propTypes = propTypes;
-
 export default ContentTitle;
