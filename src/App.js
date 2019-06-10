398c9af23c4ca8b4eedb4872e1a36c56a765ccc5
Caleb Panza <caleb.panza@icloud.org>
2019-06-10 09:15:45 -0400

    New build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from 'react';
 import './App.css';
 import {
-  Media, Loader, Content, Accordion
+  Media, Loader, Content, Accordion, Carousel
 } from './components'
 
 const imgUrl = "https://i-h1.pinimg.com/564x/bc/f8/e4/bcf8e4a9e37b32e9cdfdc775d6beeed8.jpg"
@@ -11,11 +11,30 @@ const App = () => {
   return (
     <div className="App">
 
-      <Media
-        ratio="16by9"
-        imageUrl="https://gochristfellowship.com/wp-content/uploads/2019/02/Christ-Fellowship-Church.jpg"
-        imageAlt="Image Alt"
-        rounded />
+      <Carousel>
+        <Content
+          imageAlt={imgAlt}
+          imageUrl={imgUrl}
+          ratio="1by1"
+          rounded >
+          <Content.Title>Title</Content.Title>
+          <Content.Body>body</Content.Body>
+        </Content>
+        <Content
+          imageAlt={imgAlt}
+          imageUrl={imgUrl}
+          ratio="1by1" >
+          <Content.Title>Title</Content.Title>
+          <Content.Body>body</Content.Body>
+        </Content>
+        <Content
+          imageAlt={imgAlt}
+          imageUrl={imgUrl}
+          ratio="1by1" >
+          <Content.Title>Title</Content.Title>
+          <Content.Body>body</Content.Body>
+        </Content>
+      </Carousel>
 
     </div >
   );
